2a9e970106f3ad06c16584969efd265fa994a0b6
Nicolas Louw <maben@nickbook.local>
2023-01-22 16:18:42 +1100

    removed files

--- dmenu-scripts/config.sh
+++
@@ -1,5 +0,0 @@
-### Config file for my dmenu scripts ###
-
-DMENU="dmenu -i -l 20 -p" # define dmenu prompt
-FOLDER="~/.config/.dmenu" # the folder where the dmenu scripts are located
-DMENU_APPS="dmenu -i"
